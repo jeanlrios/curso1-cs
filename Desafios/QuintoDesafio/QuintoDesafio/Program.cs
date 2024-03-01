78e9da66a2834ec759f8033aabb1ee88099b90f0
jeanlrios <161527650+jeanlrios@users.noreply.github.com>
2024-03-01 11:51:27 -0300

    Update Program.cs

--- Desafios/QuintoDesafio/QuintoDesafio/Program.cs
+++ Desafios/QuintoDesafio/QuintoDesafio/Program.cs
@@ -1,4 +1,4 @@
-﻿// 1. Criar um dicionário que represente um aluno, com uma
+// 1. Criar um dicionário que represente um aluno, com uma
 // lista de notas, e mostre a média de suas notas na tela.
 Dictionary<string, List<int>> ListaAluno = new Dictionary<string, List<int>>();
 ListaAluno.Add("Jean", new List<int>());
@@ -33,3 +33,46 @@ foreach (var Produto in Produtos)
 // 3. Crie um programa que implemente um quiz simples de perguntas e
 // respostas. Utilize um dicionário para armazenar as perguntas e as respostas
 // corretas.
+Dictionary<string, string> Perguntas = new Dictionary<string, string>();
+Perguntas["1. Quem foi a primeira pessoa a viajar no Espaço?"] = "Yuri Gagarin";
+Perguntas["2. Qual a montanha mais alta do mundo?"] = "Monte Everest";
+Perguntas["3. Onde se localiza Machu Picchu?"] = "Peru";
+
+int Acertos = 0;
+int Erros = 0;
+
+foreach (var Pergunta in Perguntas.Keys)
+{
+    Console.WriteLine(Pergunta);
+    string Resposta = Console.ReadLine();
+    if (Resposta == Perguntas[Pergunta])
+    {
+        Acertos++;
+    } else
+    {
+        Erros++;
+    };
+}
+Console.Clear();
+Console.WriteLine($"Acertos: {Acertos}");
+Console.WriteLine($"Erros: {Erros}");
+Thread.Sleep(2000);
+Console.Clear();
+
+// 4. Criar um programa que simule um sistema de login utilizando um
+// dicionário para armazenar nomes de usuário e senhas.
+Dictionary<string, string> Login = new Dictionary<string, string>();
+Login["jeanrios"] = "senha123";
+
+Console.Write("Informe seu usuario: ");
+string Usuario = Console.ReadLine();
+Console.Write("Informe sua senha: ");
+string Senha = Console.ReadLine();
+
+if (Login.ContainsKey(Usuario) && Login[Usuario] == Senha)
+{
+    Console.WriteLine("Você entrou no sistema!");
+} else
+{
+    Console.WriteLine("Usuario ou senha incorretos.");
+};
